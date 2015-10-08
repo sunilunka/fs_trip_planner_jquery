3a6931f6a6bccb208dd2cecd69fe07816932e694
Sunil Unka <sunil.unka@gmail.com>
2015-10-08 15:19:33 -0400

    Finished removing individual items from itinerary.

--- assets/scripts/tripPlanner.js
+++ assets/scripts/tripPlanner.js
@@ -17,19 +17,23 @@ $(document).ready(function() {
 
 
   $('#events .btn-circle').on('click', function(){
-    var selected = $(this).siblings('select').val();
-    var newItineraryItem = '<div class="itinerary-item"><span class="title">'+ selected +'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>'
-    var category = $(this).siblings('h4').text();
-    $('#' + category).append(newItineraryItem);
+    
+      var selected = $(this).siblings('select').val();
+      var newItineraryItem = '<div class="itinerary-item"><span class="title">'+ selected +'</span><button class="btn btn-xs btn-danger remove btn-circle">x</button></div>'
+      var category = $(this).siblings('h4').text();
+        
 
     if(days.length === 0){
-      var day = new DayItinerary()
+      var day = new DayItinerary();
       days.push(day);
-      day[category].push(selected)
+      day[category].push(selected);
+      $(".day-buttons").append(' <button class="btn btn-circle day-btn">' + (days.length) + '</button> ');
+      $('#' + category).append(newItineraryItem);
     } else {
       var dayNumber = $(document).find(".current-day").text()
       if(days[dayNumber - 1][category].indexOf(selected) === -1){
         days[dayNumber - 1][category].push(selected);
+        $('#' + category).append(newItineraryItem);
       } else {
         console.error("You have already added that event!");
       }
@@ -50,7 +54,6 @@ $(document).ready(function() {
     console.log(days);
     // First we get current-day.text() and then get days[current-day.text() - 1]
     var dayToDisplay = days[$(document).find(".current-day").text() - 1]
-    console.log(dayToDisplay);
     for(var key in dayToDisplay){
       console.log(key)
       dayToDisplay[key].forEach(function(evt){
@@ -67,6 +70,19 @@ $(document).ready(function() {
 
     $(".day-buttons").append(' <button class="btn btn-circle day-btn">' + (days.length) + '</button> ')
 
+  })
+
+  $("#day-itinerary").on("click", ".remove", function(){
+    // console.log($(this));
+      var dayToDisplay = days[$(document).find(".current-day").text() - 1]
+      var category = $(this).closest('ul').attr("id");
+      var categoryValue = $(this).siblings(".title").text();
+      if(dayToDisplay[category].indexOf(categoryValue) > -1){
+        var idx = dayToDisplay[category].indexOf(categoryValue);
+        dayToDisplay[category].splice(idx, 1);
+      }
+
+      $(this).closest(".itinerary-item").remove();
   })
 
 });
