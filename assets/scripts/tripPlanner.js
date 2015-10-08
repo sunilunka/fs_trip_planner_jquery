b722e8ef2524e8aec3f6d8ba7ef1a9c76cac073a
Sunil Unka <sunil.unka@gmail.com>
2015-10-08 17:04:02 -0400

    Finished basic map features.

--- assets/scripts/tripPlanner.js
+++ assets/scripts/tripPlanner.js
@@ -38,6 +38,48 @@ $(document).ready(function() {
         console.error("You have already added that event!");
       }
     }
+    var locations = [];
+    if(category == "Hotels"){
+      all_hotels.forEach(function(h){
+
+        if(h.name == selected){
+          locations.push({hotel: h.place[0].location});
+          
+        }
+      })
+    } else if (category == "Restaurants"){
+      all_restaurants.forEach(function(h){
+        if(h.name == selected){
+          locations.push({restaurant: h.place[0].location});
+        }
+      })
+    } else if (category == "Activities"){
+      all_activities.forEach(function(h){
+        if(h.name == selected){
+          locations.push({activity: h.place[0].location});
+        }
+      })
+    }
+
+    locations.forEach(function(obj){
+      for(var key in obj){
+        if(key === 'hotel'){
+          drawLocation(obj[key],{
+            icon: '/images/lodging_0star.png'
+          })
+        } else if(key === 'restaurant'){
+          drawLocation(obj[key],{
+            icon: '/images/restaurant.png'
+          })
+        } else if(key === 'activity'){
+          drawLocation(obj[key],{
+            icon: '/images/star-3.png'
+          })
+        }
+      }
+    });
+
+
   })
 
 
@@ -45,11 +87,15 @@ $(document).ready(function() {
     if(!$(this).hasClass("current-day")){
       $(this).siblings(".current-day").removeClass("current-day");
       $(this).addClass("current-day");
-    };
+    } 
 
+    var dayNumber = $(document).find(".current-day").text();
     $("#Hotels").empty();
     $("#Restaurants").empty();
     $("#Activities").empty();
+    marker.setMap(null);
+
+    $('#day-title span').text("Day " + dayNumber);
 
     console.log(days);
     // First we get current-day.text() and then get days[current-day.text() - 1]
@@ -67,8 +113,16 @@ $(document).ready(function() {
 
   $("#add-day-btn").on("click", function(){
     days.push(new DayItinerary());
+    // console.log($(this));
+    // if($(this).hasClass("current-day")){
+    //    console.log("#add-day-btn has current-day class")
+    //   $(this).removeClass("current-day");
+    //   console.log($(this));
+    // };
 
-    $(".day-buttons").append(' <button class="btn btn-circle day-btn">' + (days.length) + '</button> ')
+    // $(this).removeClass("current-day");
+    $(".day-buttons").append(' <button class="btn btn-circle current-day day-btn" id='+ days.length +'>' + (days.length) + '</button> ')
+    $(".day-buttons").find(".day-btn");
 
   })
 
@@ -84,5 +138,47 @@ $(document).ready(function() {
 
       $(this).closest(".itinerary-item").remove();
   })
+
+  $("#day-title").on("click", ".remove", function(){
+    // console.log($(this));
+      var dayToDisplay = days[$(document).find(".current-day").text() - 1]
+      var dayNumber = $(document).find(".current-day").text();
+      days.splice(dayNumber-1, 1);
+
+      $("#Hotels").empty();
+      $("#Restaurants").empty();
+      $("#Activities").empty();
+
+      $("#" + dayNumber ).remove();
+
+
+  });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 });
